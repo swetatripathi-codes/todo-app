03809ad71e9e757b755f73702e87cbb424dc5a99
swetatripathi-codes <sweta.shyam.narayan.tripathi@gmail.com>
2026-01-11 23:02:04 +0530

    Initial commit - React Todo App

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,31 @@
+import { TodoCard } from "./TodoCard";
+
+export function TodoList(props) {
+    const { todos, selectedTab } = props
+
+
+    const filterTodosList = selectedTab === 'All' ?
+        todos :
+        selectedTab === 'Completed' ?
+            todos.filter(val => val.complete) :
+            todos.filter(val => !val.complete)
+
+    return (
+        <>
+            {filterTodosList.map((todo, todoIndex) => {
+                const tempTodoIndex = todos.findIndex(val => val.input == todo.input)
+                console.log(tempTodoIndex)
+                // finding an index like this has it's limitations and in later courses we learn a more sophisticated way of doing it (the limitation is that the code might misbehave if you have two todos with the exact same text :) See if you can figure out why!)
+                return (
+                    <TodoCard
+                        key={todoIndex}
+                        {...props}
+                        todoIndex={tempTodoIndex}
+                        todo={todo} 
+                    />
+                )
+            })}
+
+        </>
+    )
+}
